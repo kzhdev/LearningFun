9c88950d1443913585c080d47069e403a5e3f9c0
kzhdev <kun.zhao@tradingtechnologies.com>
2015-04-05 21:16:03 -0500

    remove ignored folders

--- public/web/index.bootstrap.dart
+++
@@ -1,5 +0,0 @@
-library LearningFun.web.index_bootstrap_dart;
-
-import 'index.html.0.dart' as i0;
-
-void main() { i0.main(); }
--- public/web/index.html.0.dart
+++
@@ -1,5 +0,0 @@
-library LearningFun.web.index_html_0;
-
-      import 'package:LearningFun/app_main.dart';
-      export 'package:polymer/init.dart';
-    
